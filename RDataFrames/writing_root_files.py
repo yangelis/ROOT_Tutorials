53fc30d6bfcbfa5b44c8ca2c1545bf5ef5d6c9bd
Iaggelis <john_agelis@hotmail.com>
2019-11-16 00:40:36 +0200

    Added example of writing a root file using RDataFrame and an existing root file

---
+++ RDataFrames/writing_root_files.py
@@ -0,0 +1,17 @@
+# Let's read the same file and tree as before
+# but now just pick two branches and save the
+# in a different file.
+
+import ROOT
+from ROOT import RDataFrame
+df1 = RDataFrame('TreeS','../Data/higgs_small.root')
+
+# Sometimes when choosing the branch name 
+# python string dont work, so we are using
+# a ROOT vector of string, with the desired
+# branches.
+columns = ROOT.vector('string')() 
+columns.push_back('lepton_pT')
+columns.push_back('lepton_eta')
+# Snapshot saves in a new root file in a new Tree 
+df1.Snapshot('myTree','my_output.root',columns)
